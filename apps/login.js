735e8d1a1937bb95a253e548ef434806b7c1fd28
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-11-20 21:08:29 +0200

    Update <> login.js

--- apps/login.js
+++ apps/login.js
@@ -13,3 +13,14 @@ inPass.addEventListener('keypress',()=>{
     labelPass.classList.add('on')
     showPass.classList.add("active")
 })
+
+showPass.addEventListener('click',()=>{
+    showPass.classList.toggle('on')
+    if(inPass.type == "text"){
+        inPass.type = "password"
+    }else{
+        inPass.type = "text"
+
+    }
+
+})
