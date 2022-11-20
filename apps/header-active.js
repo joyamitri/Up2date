4355c619b951a1dbd44d9591f32e6d01796b1e40
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-11-20 21:25:45 +0200

    Create <> header-active.js

---
+++ apps/header-active.js
@@ -0,0 +1,14 @@
+// show and hide pop
+let headerImgProfile = document.querySelector('.header-img-profile')
+let profileimg = document.querySelector('.profileimg')
+let headerOverlay = document.querySelector('.header-overlay')
+
+profileimg.addEventListener('click',()=>{
+    headerImgProfile.classList.toggle('active')
+    headerOverlay.classList.toggle('active')
+})
+
+headerOverlay.addEventListener('click',()=>{
+    headerImgProfile.classList.toggle('active')
+    headerOverlay.classList.toggle('active')
+})
