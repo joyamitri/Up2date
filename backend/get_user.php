085b05d1af03832a58d424ffbdd9a28090dd1281
Joya Maria Mitri <joyamaria.mitri@lau.edu>
2022-11-27 00:34:10 +0200

    update <> backend/get_user.php

--- backend/get_user.php
+++ backend/get_user.php
@@ -1,18 +1,35 @@
 <?php
 include("connection.php");
+<<<<<<< Updated upstream
 if(isset($_GET["username"])){ 
     $username = $_GET["username"];
     $query = $mysqli -> prepare("SELECT * from users WHERE username = ?");
     $query -> bind_param("s", $username);
+=======
+
+if(isset($_GET["username"]) && isset($_GET["password"])){ 
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+    $query = $mysqli -> prepare("SELECT * from users WHERE username = ? AND password = ?");
+    $query -> bind_param("ss", $username, $password);
+>>>>>>> Stashed changes
     $query -> execute();
     $array = $query->get_result();
     $response = [];
     while($user = $array->fetch_assoc()){
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
         $response[] = $user;
     }
 
     echo json_encode($response); 
 }else{
+<<<<<<< Updated upstream
+=======
+    
+>>>>>>> Stashed changes
     die("Error");
 }
 
